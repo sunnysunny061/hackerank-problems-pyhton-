e1f5e6edd1635a3424a1cf8499f3d1efb2fd2751
sunny dhiman <68381853+sunnysunny061@users.noreply.github.com>
2021-05-30 18:34:59 +0530

    Add files via upload

---
+++ Chocolate Feast.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'chocolateFeast' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER c
+#  3. INTEGER m
+#
+
+def chocolateFeast(n, c, m):
+    # Write your code here
+    count=n//c
+    total_wrapper=n//c
+
+    for i in range(0,10):
+        rem_wrapper=total_wrapper%m
+        bar=total_wrapper//m
+        count+=bar
+        total_wrapper=bar+rem_wrapper
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        c = int(first_multiple_input[1])
+
+        m = int(first_multiple_input[2])
+
+        result = chocolateFeast(n, c, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
